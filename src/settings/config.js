53450f28da1125f3d1252f5aba33cd28652f3245
otoneko1102 <pineapple2006.11.2@gmail.com>
2026-05-06 22:14:39 +0900

    feat: add footer component with social media links and update config for URLs

--- src/settings/config.js
+++ src/settings/config.js
@@ -2,3 +2,6 @@ export const SITE_URL = 'https://gijutsusharin.li';
 export const SITE_TITLE = '技術者倫理を守ろう';
 export const SITE_DESCRIPTION = 'ちょっと待って！その発言/その行為、技術者倫理を守っていますか？';
 export const TWEET_TEXT = '技術者倫理を守ろう\nhttps://gijutsusharin.li';
+export const DISCORD_URL = 'https://discord.gg/upSpdDgDha';
+export const GITHUB_URL = 'https://github.com/otoneko1102/rinri';
+export const TWITTER_ACCOUNT = '@rin_pineapple';
