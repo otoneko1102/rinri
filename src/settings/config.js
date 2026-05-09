59cc967200bbd2d0cd30c62ae3e1a411e5366fb3
otoneko. <116952822+otnc@users.noreply.github.com>
2026-05-10 02:59:04 +0900

    chore: replace github.com/otoneko1102 with github.com/otnc

--- src/settings/config.js
+++ src/settings/config.js
@@ -3,6 +3,6 @@ export const SITE_TITLE = '技術者倫理を守ろう';
 export const SITE_DESCRIPTION = 'ちょっと待って！その発言/その行為、技術者倫理を守っていますか？';
 export const TWEET_TEXT = '技術者倫理を守ろう\nhttps://gijutsusharin.li';
 export const DISCORD_URL = 'https://discord.gg/upSpdDgDha';
-export const GITHUB_URL = 'https://github.com/otoneko1102/rinri';
+export const GITHUB_URL = 'https://github.com/otnc/rinri';
 export const TWITTER_ACCOUNT = '@rin_pineapple';
 export const ACM_CODE_URL = 'https://www.acm.org/binaries/content/assets/code-of-ethics/se-code-jpn.pdf';
